1c0dbfc1248bd968f8c79ea8726c54427ccc3739
SrikanthChowdary495 <ramasrikanth.kotaru@gmail.com>
2022-04-19 22:13:18 +0530

    fctemplate2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Section1 from './components/header';
+// import Section2 from './components/Body';
+
+function App() {
+  return (
+    <div>
+      <Section1/>
+      {/* <Section2/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+function Section1() {
+    return (
+        <div>
+            <div className="Logo"><p><b>company logo and slogan</b></p></div>
+            <div className="Head"><p><b>Business Details</b></p></div>
+            <div className="flec-c">
+                <div id="google"><a href="https://www.google.com/"><h3><b>Google</b></h3></a></div>
+                <div id="bing"><a href="https://www.bing.com/"><h3><b>Bing</b></h3></a></div>
+                <div id="firefox"><a href="https://www.mozilla.org/en-US"><h3><b>Firefox</b></h3></a></div>
+                <div id="explorer"><a href="https://www.microsoft.com/en-in/download/internet-explorer.aspx"><h3><b>Explorer</b></h3></a></div>
+            </div>
+            
+            <div className="test1">
+                <div className="test12">
+                    <div>
+                        About
+                    </div>
+                </div>
+                <div className="test13">
+                    <div className="test31">quick links</div>
+                    <div className="test32">quick links</div>
+                </div>
+            </div>
+            <div className="site">site plan and copyright info</div>
+                
+                
+        </div>
+    );
+}
+
+export default Section1
